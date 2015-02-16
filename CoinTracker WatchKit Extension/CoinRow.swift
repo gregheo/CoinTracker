153de8ebb1f4bd6d5215a83fdd76eac94013262f
Greg Heo <greg@gregheo.com>
2015-02-16 02:40:46 -0500

    WatchKit interface

--- CoinTracker WatchKit Extension/CoinRow.swift
+++ CoinTracker WatchKit Extension/CoinRow.swift
@@ -20,25 +20,12 @@
 * THE SOFTWARE.
 */
 
-import WatchKit
 import Foundation
+import WatchKit
 
-class InterfaceController: WKInterfaceController {
+class CoinRow: NSObject {
 
-  override func awakeWithContext(context: AnyObject?) {
-    super.awakeWithContext(context)
-
-    // Configure interface objects here.
-  }
-
-  override func willActivate() {
-    // This method is called when watch view controller is about to be visible to user
-    super.willActivate()
-  }
-
-  override func didDeactivate() {
-    // This method is called when watch view controller is no longer visible
-    super.didDeactivate()
-  }
+  @IBOutlet weak var nameLabel: WKInterfaceLabel!
+  @IBOutlet weak var priceLabel: WKInterfaceLabel!
 
 }
